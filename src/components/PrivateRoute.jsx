75db6e0b5af81b85ac705df95f915712474fb628
ReginaRRJ <149405875+ReginaRRJ@users.noreply.github.com>
2025-03-27 19:08:29 -0600

    cambié componentes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,5 @@
 import { Route, Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types'; // Importa PropTypes
 
 const PrivateRoute = ({ element, ...rest }) => {
   const isLoggedIn = localStorage.getItem('loggeado') === 'correcto';  // Verifica si está loggeado
@@ -9,6 +10,12 @@ const PrivateRoute = ({ element, ...rest }) => {
       element={isLoggedIn ? element : <Navigate to="/" />}
     />
   );
+};
+
+// Definición de PropTypes para el componente PrivateRoute
+PrivateRoute.propTypes = {
+  element: PropTypes.element.isRequired,  // 'element' debe ser un componente React y es requerido
+  rest: PropTypes.object, // 'rest' puede ser cualquier objeto con las propiedades adicionales (como 'path', 'exact', etc.)
 };
 
 export default PrivateRoute;
--- src/components/Texto.jsx
+++ src/components/Texto.jsx
@@ -1,6 +1,12 @@
 //import * as React from 'react'
+import PropTypes from 'prop-types'; // Importa PropTypes
 
 
 export default function ParrafoMensaje({ mensaje }) {
     return <p>{mensaje}</p>;
 }
+
+// Definición de PropTypes para el componente Subtitulo
+Subtitulo.propTypes = {
+    mensaje: PropTypes.string.isRequired,  // 'mensaje' debe ser una cadena de texto y es requerido
+  };
