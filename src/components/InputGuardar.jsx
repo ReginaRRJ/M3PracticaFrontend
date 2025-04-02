363bf4457585080e894e1340e0da75fd16cd70eb
ReginaRRJ <149405875+ReginaRRJ@users.noreply.github.com>
2025-04-01 18:08:58 -0600

    prueb 11

--- src/components/InputGuardar.jsx
+++ src/components/InputGuardar.jsx
@@ -20,7 +20,7 @@ export default function InputGuardar() {
   };
 
   return (
-    <Box sx={{ textAlign: "center", mt: 3 }}>
+    <Box component="InputGuardar" sx={{ textAlign: "center", mt: 3 }}>
       <Box>
         <Typography variant="h3" sx={{ mt: 5, paddingTop: 12, paddingBottom: 0 }}>
           Bienvenido
